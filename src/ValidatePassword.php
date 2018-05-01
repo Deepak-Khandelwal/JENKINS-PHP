68beab6bd124110f0b37afdce6cce732dad7dba9
Deepak Khandelwal <deepakkhandelwalji13@gmail.com>
2018-05-01 16:55:56 +0530

    Updated Function

--- src/ValidatePassword.php
+++ src/ValidatePassword.php
@@ -7,19 +7,5 @@ class ValidatePassword {
 
 	public function validLength($password) {
 		$passLength = strlen($password);
-
-		if($passLength >= self::MIN_LENGTH && $passLength <= self::MAX_LENGTH) {
-			return TRUE;
-		} else {
-			return FALSE;
-		}		
-	}
-
-	public function validPassword($password) {
-		if(isset($password) && $password == 'Password@123') {
-			return TRUE;
-		} else {
-			return FALSE;
-		}
 	}
 }
